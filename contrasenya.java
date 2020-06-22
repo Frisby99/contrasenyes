b9c66e65c185a8ceeeaa868cdd80342cd2648e4f
Frisby99 <67263311+Frisby99@users.noreply.github.com>
2020-06-22 13:56:41 +0200

    Update contrasenya.java

--- contrasenya.java
+++ contrasenya.java
@@ -1,7 +1,5 @@
 public class contrasenya {
-    
-public static void main(String[] args) {
-    
-    //TODO controlar les contrasenyes dels usuaris
-    
+    public static void main(String[] args) {
+        //TODO controlar les contrasenyes dels usuaris
+    }
 }
