2957014fcce5fcf0d16b51f927ce7b1d06356e50
Frisby99 <67263311+Frisby99@users.noreply.github.com>
2020-06-22 14:23:32 +0200

    Update contrasenya.java

--- contrasenya.java
+++ contrasenya.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class contrasenya {
     
-public static final int LONG_CONTRASENYA = 10;
+    public static final int LONG_CONTRASENYA = 10;
 
-public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print(
                 "Entra la contrasneya. Tingues en compte les següents restriccions:" +
@@ -34,7 +34,7 @@ public static void main(String[] args) {
             if (esNumero(c)) numNumeros++;
             else if (esLletraMajuscula(c)) numCaractersMaj++;
             else if (esLletraMinuscula(c)) numCaractersMin++;
-            else if (i <> 0 && i < password.length()-1) numSimbols++;
+            else if (i > 0 && i < password.length()-1) numSimbols++;
         }
 
         return (password.length() >= LONG_CONTRASENYA && numCaractersMaj >= 1 && numCaractersMin >= 1 && numNumeros >= 2 && numSimbols >= 1);
