e0ffe348125d3b583a91361d45cd5ef144912345
Frisby99 <67263311+Frisby99@users.noreply.github.com>
2020-06-22 13:30:06 +0200

    Update contrasenya.java

--- contrasenya.java
+++ contrasenya.java
@@ -2,18 +2,18 @@ import java.util.Scanner;
 
 public class contrasenya {
     
-public static final int LONG_CONTRASENYA = 8;
+public static final int LONG_CONTRASENYA = 10;
 
 public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
         System.out.print(
-                "Entra la contrasneya. Tingues en compte les següents restriccions:" +               "- Les contrasenyes han de tenir 10 caràcters com a mínim.\n" +
+                "Entra la contrasneya. Tingues en compte les següents restriccions:" +
+                "- Les contrasenyes han de tenir 10 caràcters com a mínim.\n" +
                 "- Hi ha d’haver almenys dos números.\n" +
                 "- Hi ha d’haver lletres en majúscules i en minúscules\n" +
                 "- Hi ha d’haver un símbol que no pot estar ni al principi ni al final\n" +
                 "Contrasenya: ");
-
         String s = input.nextLine();
 
         if (esValida(s)) {
@@ -21,35 +21,35 @@ public static void main(String[] args) {
         } else {
             System.out.println("Contrasenya NO acceptada: " + s);
         }
-
     }
 
-    public static boolean esValida(String password) {
-
-        if (password.length() < LONG_CONTRASENYA) return false;
-
-        int numCaracters = 0;
+    private static boolean esValida(String password) {
+        int numCaractersMaj = 0;
+        int numCaractersMin = 0;
         int numNumeros = 0;
+        int numSimbols = 0;
+        
         for (int i = 0; i < password.length(); i++) {
-
             char c = password.charAt(i);
 
             if (esNumero(c)) numNumeros++;
-            else if (esLletra(c)) numCaracters++;
-            else return false;
+            else if (esLletraMajuscula(c)) numCaractersMaj++;
+            else if (esLletraMinuscula(c)) numCaractersMin++;
+            else if (i <> 0 && i < password.length()-1) numSimbols++;
         }
 
-
-        return (numCaracters >= 2 && numNumeros >= 2);
+        return (password.length() >= LONG_CONTRASENYA && numCaractersMaj >= 1 && numCaractersMin >= 1 && numNumeros >= 2 && numSimbols >= 1);
     }
 
-    public static boolean is_Letter(char ch) {
-        ch = Character.toUpperCase(ch);
-        return (ch >= 'A' && ch <= 'Z');
+    private static boolean esLletraMajuscula(char c) {
+        return (c >= 'A' && c <= 'Z');
     }
 
-    public static boolean esNumero(char c) {
+    private static boolean esLletraMinuscula(char c) {
+        return (c >= 'a' && c <= 'z');
+    }
 
+    private static boolean esNumero(char c) {
         return (c >= '0' && c <= '9');
     }
 }
