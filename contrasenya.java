d74ac1a0c8fa3fae6477fee437b3dcd066848ec8
Frisby99 <67263311+Frisby99@users.noreply.github.com>
2020-06-22 14:00:11 +0200

    Update contrasenya.java

--- contrasenya.java
+++ contrasenya.java
@@ -1,46 +1,46 @@
 import java.util.Scanner;
 
 public class contrasenya {
-    
-public static final int LONG_CONTRASENYA = 8;
+    public static final int LONG_CONTRASENYA = 8;
 
-public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print(
-                "Entra la contrasneya. Tingues en compte les següents restriccions: \n" +
-                "- Les contrasenyes han de tenir 8 caràcters com a mínim.\n" +
-                "- Hi ha d’haver lletres.\n" +
-                "- Hi ha d’haver algun número\n" +
-                "Contrasenya: ");
-        String s = input.nextLine();
+    public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+            System.out.print(
+                    "Entra la contrasneya. Tingues en compte les següents restriccions: \n" +
+                    "- Les contrasenyes han de tenir 8 caràcters com a mínim.\n" +
+                    "- Hi ha d’haver lletres.\n" +
+                    "- Hi ha d’haver algun número\n" +
+                    "Contrasenya: ");
+            String s = input.nextLine();
 
-        if (esValida(s)) {
-            System.out.println("Contrasenya acceptada: " + s);
-        } else {
-            System.out.println("Contrasenya NO acceptada: " + s);
-        }
-    }
-
-    public static boolean esValida(String password) {
-        int numCaracters = 0;
-        int numNumeros = 0;
-        for (int i = 0; i < password.length(); i++) {
-            char c = password.charAt(i);
-
-            if (esNumero(c)) numNumeros++;
-            else if (esLletra(c)) numCaracters++;
-            else return false;
+            if (esValida(s)) {
+                System.out.println("Contrasenya acceptada: " + s);
+            } else {
+                System.out.println("Contrasenya NO acceptada: " + s);
+            }
         }
 
-        return (password.length() >= LONG_CONTRASENYA && numCaracters >= 1 && numNumeros >= 1);
-    }
+        public static boolean esValida(String password) {
+            int numCaracters = 0;
+            int numNumeros = 0;
+            for (int i = 0; i < password.length(); i++) {
+                char c = password.charAt(i);
 
-    public static boolean esLletra(char c) {
-        c = Character.toUpperCase(c);
-        return (c >= 'A' && c <= 'Z');
-    }
+                if (esNumero(c)) numNumeros++;
+                else if (esLletra(c)) numCaracters++;
+                else return false;
+            }
 
-    public static boolean esNumero(char c) {
-        return (c >= '0' && c <= '9');
+            return (password.length() >= LONG_CONTRASENYA && numCaracters >= 1 && numNumeros >= 1);
+        }
+
+        public static boolean esLletra(char c) {
+            c = Character.toUpperCase(c);
+            return (c >= 'A' && c <= 'Z');
+        }
+
+        public static boolean esNumero(char c) {
+            return (c >= '0' && c <= '9');
+        }
     }
 }
